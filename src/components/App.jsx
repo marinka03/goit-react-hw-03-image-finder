9e12073c2805c3ba9fe3c573a215c189d79392e3
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-06-19 02:16:43 +0300

    create-file-for-component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import style from '../components/App.module.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className={style.App}>
+        image-finder
+      </div>
+    )
+  }
+}
+
+export default App;
