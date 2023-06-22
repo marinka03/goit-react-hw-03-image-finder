ebd3a66d90aa20ca8fc37e86a0843ad8daa8ace5
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-06-22 23:44:51 +0300

    create-all-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,92 @@
 import React, { Component } from 'react';
-import style from '../components/App.module.css'
+import { searchPhotoApi } from 'helpers/api';
+import style from '../components/App.module.css';
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import Modal from './Modal';
 
 class App extends Component {
+  state = {
+    images: [],
+    currentPage: 1,
+    SearchValue: '',
+    errorMessage: '',
+    isFetching: false,
+    modal: { isOpen: false, src: '', alt: '' },
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.currentPage !== this.state.currentPage ||
+      prevState.SearchValue !== this.state.SearchValue
+    ) {
+      this.requestListImages(this.state.SearchValue, this.state.currentPage);
+    }
+  }
+
+  requestListImages = (SearchValue, currentPage) => {
+    this.setState(() => ({ isFetching: true }));
+
+    setTimeout(async () => {
+      try {
+        const { hits } = await searchPhotoApi(SearchValue, currentPage);  //ЗАПИТ НА СЕРВЕР
+        if (currentPage === 1) {
+          this.setState(() => ({ images: hits }));
+        } else {
+          this.setState(state => ({
+            images: [...state.images, ...hits],
+          }));
+        }
+      } catch (error) {
+        this.setState(() => ({ errorMessage: error.message }));
+      } finally {
+        this.setState(() => ({ isFetching: false }));
+      }
+    }, 1000);
+  };
+
+  hendleSubmitForm = nameFromForm => {
+    this.setState({ SearchValue: nameFromForm }); //   CHANGE STATE
+  };
+
+  handleLoadMoreBtn = () => {
+    this.setState(state => ({ currentPage: state.currentPage + 1 })); //PAGINATION
+  };
+
+  handleOpenModal = (src, alt) => {
+    this.setState({ modal: { isOpen: true, src, alt } });
+  };
+
+  handleCloseModal = () => {
+    this.setState(() => ({ modal: { isOpen: false, src: '', alt: '' } }));
+  };
+
   render() {
+    const {
+      images,
+      errorMessage,
+      isFetching,
+      currentPage,
+      modal: { src, alt },
+    } = this.state;
+
     return (
       <div className={style.App}>
-        image-finder
+        <Searchbar onSubmit={this.hendleSubmitForm} />
+        <ImageGallery
+          error={errorMessage}
+          isFetching={isFetching}
+          images={images}
+          openFullScreenMode={this.handleOpenModal}
+          currentPage={currentPage}
+          handleLoadMoreBtn={this.handleLoadMoreBtn}
+        />
+
+        {this.state.modal.isOpen && (
+          <Modal closeModal={this.handleCloseModal} src={src} alt={alt} />
+        )}
       </div>
-    )
+    );
   }
 }
 
